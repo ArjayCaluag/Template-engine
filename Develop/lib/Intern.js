2b0ae8e56bb09e767279852119f6a70167cbbf2d
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-22 20:08:41 -0800

    Add intern and manager classes

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+//Requiring Employee.js file
+const Employee = require("./Employee")
+
+// Extension of parent class Employee
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        //  super used to call the Employee constructor function
+        super(name, id, email)
+        this.school = school
+    }
+
+    getSchool() {
+        return this.school
+    }
+
+    getRole() {
+        return 'Intern'
+    }
+}
+
+module.exports = Intern;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,28 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+//Requiring Employee.js file
+const Employee = require("./Employee")
+
+// Extension of parent class Employee
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        
+        //  super used to call the Employee constructor function
+        super(name, id, email)
+        this.officeNumber = officeNumber
+    }
+
+    officeNumber() {
+        return this.officeNumber
+    }
+
+    getRole() {
+        return 'Manager'
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber
+    }
+}
+
+module.exports = Manager;

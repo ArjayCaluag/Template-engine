ac4ab8929f849892f3781838e7800cc790f6ebc8
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-22 19:56:44 -0800

    Add Engineer class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -4,26 +4,26 @@
 class Employee {
     constructor(name, id, email) {
         this.name = name,
-        this.id = id,
-        this.email = email
+            this.id = id,
+            this.email = email
     }
 
     // get functions to retrn
-getName(){
-    return this.name;
-}
+    getName() {
+        return this.name;
+    }
 
-getId(){
-    return this.id
-}
+    getId() {
+        return this.id
+    }
 
-getEmail(){
-    return this.email
-}
+    getEmail() {
+        return this.email
+    }
 
-getRole(){
-    return 'Employee'
-}
+    getRole() {
+        return 'Employee'
+    }
 
 }
 
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+//Requiring Employee.js file
+const Employee = require("./Employee")
+
+// Extension of parent class Employee
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        //  super used to call the Employee constructor function
+        super(name, id, email)
+        this.github = github
+    }
+
+    getGithub() {
+        return this.github
+    }
+
+    getRole() {
+        return 'Engineer'
+    }
+}
+
+module.exports = Engineer;

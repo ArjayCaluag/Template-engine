c09033be08d27a676369ec86a79e632a15909445
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-22 19:51:46 -0800

    Add parent Employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+
+// Parent class that will be shared with engineer, intern, and manager js files.
+class Employee {
+    constructor(name, id, email) {
+        this.name = name,
+        this.id = id,
+        this.email = email
+    }
+
+    // get functions to retrn
+getName(){
+    return this.name;
+}
+
+getId(){
+    return this.id
+}
+
+getEmail(){
+    return this.email
+}
+
+getRole(){
+    return 'Employee'
+}
+
+}
+
+// Export making available to other files.
+module.exports = Employee;
